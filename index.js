4592f822f884e488ea507f031f68b57cd97cf258
RollViral <51767087+tyler-Github@users.noreply.github.com>
2023-03-31 18:38:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const markio = require("markio");
+const path = require('path');
 
 const app = express();
 
 app.use(markio.markioMiddleware);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
   res.locals.data = "# Hello, world!";
-  res.render("index");
+  const renderedMarkdown = markio.renderMarkdown(res.locals.data);
+  res.render("index", { renderedMarkdown });
 });
 
 app.get("/file", (req, res) => {
   res.locals.data = { filename: "example.md" };
-  res.render("index");
+  fs.readFile(res.locals.data.filename, "utf-8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Error reading file");
+    }
+    const renderedMarkdown = markio.renderMarkdown(data);
+    res.render("index", { renderedMarkdown });
+  });
+});
+
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
